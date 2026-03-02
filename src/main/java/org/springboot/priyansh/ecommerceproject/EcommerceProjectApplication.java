f107060b3a8bfa10aac065f394b6c4dbb6d05544
priyansh-awasthi-TTN <priyansh.awasthi@tothenew.com>
2026-03-02 17:17:44 +0530

    Init

---
+++ src/main/java/org/springboot/priyansh/ecommerceproject/EcommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.priyansh.ecommerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceProjectApplication.class, args);
+    }
+
+}
