17705830120798965dba73d642c5d0b951ffa10f
priyansh-awasthi-TTN <priyansh.awasthi@tothenew.com>
2026-03-10 18:11:58 +0530

    Refactor

--- Ecommerce-Project/src/main/java/com/example/ecommerceproject/enums/PaymentMethodEnums.java
+++
@@ -1,9 +0,0 @@
-package com.example.ecommerceproject.enums;
-
-public enum PaymentMethodEnums {
-    UPI,
-    CARD,
-    COD,
-    NET_BANKING,
-    WALLET
-}
--- Ecommerce-Project/src/main/java/com/example/ecommerceproject/enums/RoleEnums.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerceproject.enums;
-
-public enum RoleEnums {
-    ROLE_CUSTOMER,
-    ROLE_SELLER,
-    ROLE_ADMIN
-}
